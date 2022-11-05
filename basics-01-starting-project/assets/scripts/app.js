15f147da724af4a75b4ea96ecb8ca2b4f74b3436
Abhi16DTU <abhi160197@gmail.com>
2022-11-05 20:12:09 +0530

    changes

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,17 +1,53 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
-
+let logEntries = [];
 // currentResult = currentResult + 10*3;
+
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription); // from vendor file
+}
 
 
 function add() {
-    currentResult = currentResult + parseInt(userInput.value) ;
-    // can use + instead of parseInt
-    // alert('The result is ' + result);
-    outputResult(currentResult, '');
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    logEntries.push(enteredNumber);
+    createAndWriteOutput('+', initialResult, enteredNumber);
+    console.log(logEntries);
 }
-addBtn.addEventListener('click', add);
 
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber);
+}
+
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
 
 
