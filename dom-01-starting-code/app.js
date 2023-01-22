57a0c7904c76e0dba14501c7dc3e0dff9ae6c60a
Abhi16DTU <abhi160197@gmail.com>
2023-01-22 22:56:44 +0530

    added dom stuff

---
+++ dom-01-starting-code/app.js
@@ -0,0 +1,22 @@
+// window.alert("Hello, world!");
+// console.log(document);
+
+
+const h1 = document.querySelector('h1');
+
+h1.textContent = 'Hello, world!';
+h1.style.color = 'red';
+h1.style.backgroundColor = 'black';
+
+const li = document.querySelector('li:last-of-type');
+li.textContent = li.textContent + ' (changed!)';
+
+const body = document.body;
+
+
+const listItemElements = document.querySelectorAll('li');
+
+for (const listItemEl of listItemElements) {
+    console.dir(listItemEl);
+    }
+
