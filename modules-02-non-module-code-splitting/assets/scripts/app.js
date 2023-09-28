d51fa27ac141d2033c73ea2d4e9b111059f439be
Abhinav Singh <abhinavsingh@Abhinavs-MacBook-Air.local>
2023-09-28 09:36:06 +0530

    added eslint

--- modules-02-non-module-code-splitting/assets/scripts/app.js
+++ modules-02-non-module-code-splitting/assets/scripts/app.js
@@ -1,16 +1,15 @@
-import { ProjectList } from "./App/ProjectList.js";
-
+import { ProjectList } from './App/ProjectList.js'
 
 class App {
-  static init() {
-    const activeProjectsList = new ProjectList('active');
-    const finishedProjectsList = new ProjectList('finished');
+  static init () {
+    const activeProjectsList = new ProjectList('active')
+    const finishedProjectsList = new ProjectList('finished')
     activeProjectsList.setSwitchHandlerFunction(
       finishedProjectsList.addProject.bind(finishedProjectsList)
-    );
+    )
     finishedProjectsList.setSwitchHandlerFunction(
       activeProjectsList.addProject.bind(activeProjectsList)
-    );
+    )
 
     // const timerId = setTimeout(this.startAnalytics, 3000);
 
@@ -19,12 +18,12 @@ class App {
     // });
   }
 
-  static startAnalytics() {
-    const analyticsScript = document.createElement('script');
-    analyticsScript.src = 'assets/scripts/Utility/Analytics.js';
-    analyticsScript.defer = true;
-    document.head.append(analyticsScript);
+  static startAnalytics () {
+    const analyticsScript = document.createElement('script')
+    analyticsScript.src = 'assets/scripts/Utility/Analytics.js'
+    analyticsScript.defer = true
+    document.head.append(analyticsScript)
   }
 }
 
-App.init();
+App.init()
