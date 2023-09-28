229d497a44b7c2af452484ccae8da69fe2486c66
Abhinav Singh <abhinavsingh@Abhinavs-MacBook-Air.local>
2023-09-28 19:24:11 +0530

    eslint and webpack

---
+++ modules-02-non-module-code-splitting/webpack.config.js
@@ -0,0 +1,21 @@
+/* eslint-disable semi */
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'assets', 'scripts'),
+    publicPath: './assets/scripts/',
+    clean: true
+  },
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname)
+    },
+    compress: true,
+    port: 9000
+  },
+};
